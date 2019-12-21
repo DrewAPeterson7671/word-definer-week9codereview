6651277b41503418a1908dd0d8f9697f82da01ab
Student <example@example.com>
2019-12-20 16:41:03 -0800

    Add app page and erb pages

--- app.rb
+++ app.rb
@@ -23,3 +23,60 @@ end
 get('/words/new') do
   erb(:new_word)
 end
+
+post('/words') do
+  name = params[:word_name]
+  word = Word.new(word_name, nil)
+  word.save()
+  redirect to ('/words')
+end
+
+
+get ('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
+end
+
+get ('/words/:id/edit') do
+  @word = Word.find(params[:id].to_i())
+  erb(:edit_word)
+end
+
+patch ('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.update(params[:name])
+  redirect to('/words')
+end
+
+
+delete ('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.delete()
+  redirect to('/words')
+end
+
+get ('/words/:id/definitions/:definition_id') do
+  @definition = Definition.find(params[:definition_id].to_i())
+  erb(:definition)
+end
+
+post ('/words/:id/definitions') do
+  @word = Word.find(params[:id].to_i())
+  definition = Definition.new(params[:definition_name], @word.id, nil)
+  definition.save()
+  erb(:word)
+end
+
+patch ('/words/:id/definitions/:definition_id') do
+  @word = Word.find(params[:id].to_i())
+  definition = Definition.find(params[:definition_id].to_i())
+  definition.update(params[:name], @word.id)
+  erb(:word)
+end
+
+delete ('/words/:id/definitions/:definition_id') do
+  definition = Definition.find(params[:definition_id].to_i())
+  definition.delete
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
+end
